aba13ba7651ab7e0b2fd763e5352e085e6002641
Yornik Heyl <yornik@yornik.nl>
2018-03-16 14:08:17 +0100

    Adding tables with php becuese why not.

---
+++ addtable.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "faqapp";
+$password = "bmS7GXQPLaJrFvxgZMBM8TvJQXAN9dknK2R3RU4DSmYALT84sTz6aqsHqvJQS6efRVAFYs";
+$dbname = "faqapp";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE Users (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(30) NOT NULL,
+passwd VARCHAR(30) NOT NULL,
+salt VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table Users created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once('Database.php');
 
+//reading the database (should make a install script so you can change the password and db/username.
 $db = new Database(faqapp, faqapp, bmS7GXQPLaJrFvxgZMBM8TvJQXAN9dknK2R3RU4DSmYALT84sTz6aqsHqvJQS6efRVAFYs);
 $QAarray = $db->select(QA,"id != 0", 200, 'category ASC')->result_array();
 
